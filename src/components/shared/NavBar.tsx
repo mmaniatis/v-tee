299dee1828c25d50f25790e5decb741f06f255a2
Michael Maniatis <michaelmaniatis@Michaels-MacBook-Pro.local>
2024-11-19 21:05:32 -0600

    making nav bar follow color scheme

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     <nav className="bg-white border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <Link href={homeLink} className="flex items-center text-green-600 font-bold text-xl">
+          <Link href={homeLink} className="flex items-center font-bold text-xl" style={{color: business != null ? business.uiSettings.colors.secondary : '#43A047'}}>
             <Home className="w-6 h-6 mr-2" />
             {homeText}
           </Link>
